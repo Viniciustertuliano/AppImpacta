54aa10e4eedf58f9721c1cc4da0ce59292ec46bb
Vinicius Tertuliano da silva <viniciustertulianodasilva@hotmail.com>
2021-03-17 15:03:31 -0300

    AppGasolinaouAlcool (entendo input no kotlin)

--- app/src/main/java/com/example/meuprimeiroapp/MainActivity.kt
+++ app/src/main/java/com/example/meuprimeiroapp/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.meuprimeiroapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.TextView
+import android.widget.EditText
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,9 +13,28 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
-    fun click(v:View) {
-        var text01 = findViewById(R.id.tv_ola) as TextView
+    fun calcular(v:View){
+        val alcool: EditText = findViewById(R.id.et_valorAlcool)
+        val gasolina: EditText = findViewById(R.id.et_valorGasolina)
 
-        text01.setText("Olá Mundo!")
+        var relacao: Double
+
+        val valoralcool:String? = alcool.text.toString()
+        val valorgasolina:String? = gasolina.text.toString()
+
+        if (valoralcool != null && valoralcool!= "" &&
+            valorgasolina != null && valorgasolina != ""){
+            relacao = valoralcool.toDouble() / valorgasolina.toDouble()
+
+            if (relacao < 0.7) {
+                Toast.makeText(this, "Abasteça com Alcool", Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(this, "Abasteça com Gasolina", Toast.LENGTH_LONG).show()
+            }
+        }
+        else{
+            Toast.makeText(this, "Digite o valor correto", Toast.LENGTH_LONG).show()
+        }
     }
+
 }
